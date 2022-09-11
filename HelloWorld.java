7bd17de82e6ad8f8640a8d248cea0ef72a8e93d6
Inna Kotenko <ikotenk0@email.cpcc.edu>
2022-09-11 14:31:54 -0500

    Revert "changed"
    This reverts commit ad9e03ea38d308833e3e1e3213d20533f3547264.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,6 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello world!");
-      
-      //statement is a reference variable
-      String statement = "Program modified, but able to revert to original version using GitHub Desktop.”;
-      
-      System.out.println(statement);
-      
-   }//end main
-}//end class
+      System.out.println("Hello world!")
+   }
+}
