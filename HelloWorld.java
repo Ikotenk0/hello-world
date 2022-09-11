ad9e03ea38d308833e3e1e3213d20533f3547264
Inna Kotenko <ikotenk0@email.cpcc.edu>
2022-09-11 14:29:52 -0500

    changed
    added the statement that program can be modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,12 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello world!")
-   }
-}
+      System.out.println("Hello world!");
+      
+      //statement is a reference variable
+      String statement = "Program modified, but able to revert to original version using GitHub Desktop.”;
+      
+      System.out.println(statement);
+      
+   }//end main
+}//end class
